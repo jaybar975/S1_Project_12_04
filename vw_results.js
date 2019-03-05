1db91aa78235c7a029cb0d8fc017ff010b57426b
Justin Aybar <jaybar975@west-mec.org>
2019-03-05 15:05:30 -0700

    Final Commit Push-mReady for Review
    Added a switch statement, functions, for loops, and variables to the Javascript file.

--- vw_results.js
+++ vw_results.js
@@ -24,15 +24,46 @@
       
 */
 var reportHTML = "<h1>" + raceTitle + "</h1>";
-for (var i = 0; race = 0; i++) {
+for (var i = 0; i < race.length; i++) {
     var totalVotes = 0;
-    vertext[i].forEach(calcSum());
+    votes[i].forEach(calcSum);
     reportHTML += "<table><caption>" + race[i] + "</caption><tr><th>Candidate</th><th>Votes</th></tr>";
-    reportHTML += candidateRows([i], totalVotes);
+    reportHTML += candidateRows(i, totalVotes);
+    reportHTML += "</table>";
+}
+document.getElementsByTagName("section")[0].innerHTML = reportHTML;
 
+function candidateRows(raceNum, totalVotes) {
+    var rowHTML = "";
+    for (var j = 0; j <= 2; j++) {
+        var candidateName = candidate[raceNum][j];
+        var candidateParty = party[raceNum][j];
+        var candidateVotes = votes[raceNum][j];
+        var candidatePercent = calcPercent(candidateVotes, totalVotes);
+        rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + ")</td>";
+        for (var k = 0; k <= candidatePercent; k++) {
+            rowHTML += createBar(candidateParty, candidatePercent);
+        }
+        rowHTML += "</tr>";
+    }
+    return rowHTML;
 }
 
-
+function createBar(partyType) {
+    var barHTML = "";
+    switch (partyType) {
+        case "D":
+            barHTML = "<td class='dem'></td>";
+            break;
+        case "R":
+            barHTML = "<td class='rep'></td>";
+            break;
+        case "I":
+            barHTML = "<td class='ind'></td>";
+            break;
+    }
+    return barHTML;
+}
 
 /* Callback Function to calculate an array sum */
 function calcSum(value) {
